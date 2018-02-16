22aaa21318bb0f02d708106156cea57bbc324caf
JohnBeckner <jbeckner3@gatech.edu>
2018-02-15 21:15:06 -0500

    Added android app created in M4

---
+++ app/src/main/java/com/example/johnbeckner/buzzshelter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.johnbeckner.buzzshelter;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder logoutAlt = new AlertDialog.Builder(MainActivity.this);
+                logoutAlt.setTitle("Logout?");
+                logoutAlt.setMessage("Do you want to log out?").setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // dismiss the alert
+                            }
+                        });
+                AlertDialog alert = logoutAlt.create();
+                alert.show();
+            }
+        });
+    }
+
+}
