7e9548599d12158a22710442ab89b4ab766c15f2
Daneolog <daneolog@gmail.com>
2018-04-19 16:53:57 -0400

    Added mail code...? Still has syntax errors, so don't pull from this branch

--- app/src/main/java/com/example/johnbeckner/buzzshelter/ForgotActivity.java
+++ app/src/main/java/com/example/johnbeckner/buzzshelter/ForgotActivity.java
@@ -1,6 +1,10 @@
 package com.example.johnbeckner.buzzshelter;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +13,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Properties;
 import java.util.Random;
 
 public class ForgotActivity extends AppCompatActivity {
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,52 @@ public class ForgotActivity extends AppCompatActivity {
         goButton.setOnClickListener(v -> {
             Random rand = new Random(System.currentTimeMillis());
             int randomNumber = rand.nextInt(999999 - 111110) + 111111;
+
+            final String username = "cs2340tempemail@gmail.com";
+            final String password = "CS2340TempPassword";
+
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("from-email@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse("to-email@gmail.com"));
+                message.setSubject("Testing Subject");
+                message.setText("Dear Mail Crawler,"
+                        + "\n\n No spam to my email, please!");
+
+                MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+                Multipart multipart = new MimeMultipart();
+
+                messageBodyPart = new MimeBodyPart();
+                String file = "path of file to be attached";
+                String fileName = "attachmentName"
+                DataSource source = new FileDataSource(file);
+                messageBodyPart.setDataHandler(new DataHandler(source));
+                messageBodyPart.setFileName(fileName);
+                multipart.addBodyPart(messageBodyPart);
+
+                message.setContent(multipart);
+
+                Transport.send(message);
+
+                System.out.println("Done");
+
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
 
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Please enter the number you received in your email:");
